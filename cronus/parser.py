4274d7b0fb42cbc2ca371f3428819ab4c9e2746a
Daniel Edgecombe <edgy@qad.re>
2019-06-27 13:28:27 +0100

    build the parser and implement render

---
+++ cronus/parser.py
@@ -0,0 +1,81 @@
+section_config = [
+    ("minute", 0, 59),  # minutes allowed values
+    ("hour", 0, 23),  # hours allowed values
+    ("day of month", 0, 30, True),  # day of month allowed values
+    ("month", 0, 11, True),  # month allowed values
+    ("day_of_week", 0, 6),  # day of week allowed values
+]
+
+
+class CronParser:
+    def __init__(self, crontab, command):
+        self._crontab = crontab
+        self._command = command
+        self._parsed_values = {}
+
+    def parse(self):
+        sections = self._crontab.split()
+        if len(sections) != 5:
+            raise SyntaxError("Invalid crontab")
+
+        for i, section in enumerate(sections):
+            type_ = section_config[i][0]
+            self._parsed_values[type_] = parse_section(section, *section_config[i][1:])
+
+    def render(self):
+        for config in section_config:
+            print(config[0].ljust(15), " ".join([str(v) for v in self._parsed_values[config[0]]]))
+        print("command".ljust(15), self._command)
+
+
+def parse_section(section, min_range, max_range, one_index=False):
+    pieces = section.split(",")
+    values = set()
+    for piece in pieces:
+        piece, step, has_step, is_range = parse_piece(piece)
+
+        start, stop = extract_range(piece, has_step, is_range, one_index, min_range, max_range)
+
+        for i in range(start, stop + 1):
+            if step is None or i % step == 0:
+                if one_index:
+                    values.add(i + 1)
+                else:
+                    values.add(i)
+
+    return values
+
+
+def parse_piece(piece):
+    has_step = "/" in piece
+    is_range = "-" in piece
+    step = None
+    if has_step:
+        piece, step = piece.split("/")
+        step = int(step)
+
+    return piece, step, has_step, is_range
+
+
+def extract_range(piece, has_step, is_range, one_index, min_range, max_range):
+    start, stop = 0, 0
+    if is_range:
+        start, stop = piece.split("-")
+        if one_index:
+            start, stop = int(start) - 1, int(stop) - 1
+    elif piece == "*":
+        start, stop = min_range, max_range
+    else:
+        start = piece
+        stop = piece
+        if has_step:
+            stop = max_range
+        if one_index:
+            start, stop = int(start) - 1, int(stop) - 1
+
+    return int(start), int(stop)
+
+
+parser = CronParser("*/15 0 1,15 * 1-5", "/usr/bin/find")
+parser.parse()
+parser.render()
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import codecs
+import os
+
+import setuptools
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding="utf-8").read()
+
+
+setuptools.setup(
+    name="cronus",
+    version="0.0.1",
+    author="Daniel Edgecombe",
+    author_email="edgy.edgemond@gmail.com",
+    maintainer="Daniel Edgecombe",
+    maintainer_email="edgy.edgemond@gmail.com",
+    license="Apache Software License 2.0",
+    url="https://github.com/EdgyEdgemond/cronus",
+    description="A crontab parser",
+    long_description=read("README.rst"),
+    packages=setuptools.find_packages(include=("cronus*",)),
+    python_requires=">=3.6",
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    entry_points={
+    },
+    extras_require={
+        "dev": (
+            "flake8",
+            "flake8-commas",
+            "flake8-isort",
+            "flake8-quotes",
+            "isort>=4.3.15",
+        ),
+        "test": (
+            "pytest-cov",
+            "pytest-random-order",
+            "pytest-xdist",
+        ),
+    },
+)
